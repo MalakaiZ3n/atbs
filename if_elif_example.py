828cec09e745878d77bf4e61b557a03cfe584020
Blake Nickens <37607748+blakenick77@users.noreply.github.com>
2020-01-13 06:54:22 +0800

    if statements

--- if_elif_example.py
+++ if_elif_example.py
@@ -5,6 +5,6 @@ if name == 'Alice':
 elif age < 12:
     print('You are not Alice, kiddo.')
 elif age > 2000:
-    print('Unlike you, Alice is not an undead, immortal vampire.')
+    print('Unlike you, Alice is not an undead, immortal vampire')
 elif age > 100:
     print('You are not Alice, grannie.')
